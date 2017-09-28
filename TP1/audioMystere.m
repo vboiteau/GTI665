cb788d63e4c896d09faa30c81c8709902eb42bbc
Andres Ponce de Leon <andponcedeleon@gmail.com>
2017-09-28 19:24:56 -0400

    Partie 3 finie, 2 fichiers mysteres + graphiques

--- TP1/audioMystere.m
+++ TP1/audioMystere.m
@@ -1,22 +1,58 @@
-[s1,F1] = audioread('media/mystere1.wav');
-figure(1),plot(s1), title('Mystere 1');
-xlim([0 F1]);
-duration1 = length(s1)/F1; 
-n = length(s1);     % Total number of samples
-t = (0:n-1)/F1;      % Time for entire signal
-fprintf('Total number of samples = %f \n',n);
-fprintf('S1 duration = %f seconds \n',duration1);
-fprintf('Frequency = %f \n',F1);
+% Documentation Matlab Butterworth Filter:
+% https://www.mathworks.com/help/signal/ref/butter.html
 
-% Filtre pour le premier fichier mystere
-% https://www.mathworks.com/matlabcentral/answers/317340-how-to-filtre-an-audio-signal-with-low-pass-filtre
-fc = 1300; % Make higher to hear higher frequencies.
+% FICHIER MYSTERE 1
+[s1,F1] = audioread('media/mystere1.wav');
+% Figure 1 Mystere 1 - Fichier Entier
+figure(1),plot(s1), title('Mystere 1 - Fichier entier');
+% Figure 2 Mystere 1 - Seconde du Signal
+figure(2),plot(s1), title('Mystere 1 - Premiere Seconde');
+xlim([0 F1]);
+% Figure 3 Mystere 1 - Spectrogramme
+figure(3),specgram(s1, 512, F1), title('Mystere 1 - Spectrogramme');
+
+% Mystere 1 - Filtre 
+fc = 1300;
 % Design a Butterworth filter.
 [b, a] = butter(6,fc/(F1/2));
-figure(2),freqz(b,a), title('Mystere 1');
 % Apply the Butterworth filter.
 filteredSignal = filter(b, a, s1);
-figure(3),plot(filteredSignal), title('Mystere Filtree 1');
+% Figure 4 Mystere 1 Filtre - Fichier Entier
+figure(4),plot(filteredSignal), title('Mystere 1 Filtre - Fichier entier');
+% Figure 5 Mystere 1 Filtre - Seconde du Signal
+figure(5),plot(filteredSignal), title('Mystere 1 Filtre - Premiere Seconde');
 xlim([0 F1]);
-% Play the sound.
+% Figure 6 Mystere 1 Filtre - Spectrogramme
+figure(6),specgram(filteredSignal, 512, F1), title('Mystere 1 Filtre - Spectrogramme');
+% Jouer le signal flitre
 sound(filteredSignal*80,F1);
+% Ecrire le fichier de sortie (voix claire)
+audiowrite('mystere1_voix.wav',filteredSignal*80,F1);
+
+% FICHIER MYSTERE 2
+[s2,F2] = audioread('media/mystere2.wav');
+% Figure 7 Mystere 2 - Fichier Entier
+figure(7),plot(s2), title('Mystere 2 - Fichier entier');
+% Figure 8 Mystere 2 - Seconde du Signal
+figure(8),plot(s2), title('Mystere 2 - Premiere Seconde');
+xlim([0 F2]);
+% Figure 9 Mystere 2 - Spectrogramme
+figure(9),specgram(s2, 512, F2), title('Mystere 2 - Spectrogramme');
+
+% Mystere 2 - Filtre 
+fc = 350;
+% Design a Butterworth filter.
+[b, a] = butter(6,fc/(F2/2),'high');
+% Apply the Butterworth filter.
+filteredSignal2 = filter(b, a, s2);
+% Figure 4 Mystere 1 Filtre - Fichier Entier
+figure(10),plot(filteredSignal2), title('Mystere 2 Filtre - Fichier entier');
+% Figure 5 Mystere 1 Filtre - Seconde du Signal
+figure(11),plot(filteredSignal2), title('Mystere 2 Filtre - Premiere Seconde');
+xlim([0 F1]);
+% Figure 6 Mystere 1 Filtre - Spectrogramme
+figure(12),specgram(filteredSignal2, 512, F2), title('Mystere 2 Filtre - Spectrogramme');
+% Jouer le signal flitre.
+sound(filteredSignal2*50,F2);
+% Ecrire le fichier de sortie (voix claire)
+audiowrite('mystere2_voix.wav',filteredSignal2*50,F1);
