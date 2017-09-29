4c85ddd965cc775dd00e2b9be9251a15507b3416
Boiteau-Robert <vincent.boiteau-robert.1@ens.etsmtl.ca>
2017-09-28 20:01:36 -0400

    detect most frequent frequence with mode

--- TP1/detecteur.m
+++ TP1/detecteur.m
@@ -1,28 +1,44 @@
-[signal1] = audioread('numeros_mysteres/numero_mystere1.wav');
-phoneNumber1 = getPhoneNumberFromSignal(signal1);
-fprintf('phoneNumber1');
-disp(phoneNumber1);
+getPhoneNumberByIndex(1);
+getPhoneNumberByIndex(2);
+getPhoneNumberByIndex(3);
+getPhoneNumberByIndex(4);
+getPhoneNumberByIndex(5);
 
-[signal2] = audioread('numeros_mysteres/numero_mystere2.wav');
-phoneNumber2 = getPhoneNumberFromSignal(signal2);
+function [phoneNumber] = getPhoneNumberByIndex(index)
+    [signal, frequence] = audioread(sprintf('numeros_mysteres/numero_mystere%d.wav', index));
+    valid = 1;
+    for echantillonsParBloc = [640, 512, 320, 256, 192, 128, 96, 64, 32, 16, 8, 4, 2]
+        blocsParSeconde = frequence / echantillonsParBloc;
+        phoneNumber = getPhoneNumberFromSignal(signal, echantillonsParBloc, blocsParSeconde);
+        if echantillonsParBloc == 256
+            valid = 0;
+        end
+        if length(phoneNumber) == 10
+            break;
+        end
+    end
 
-fprintf('phoneNumber2');
-disp(phoneNumber2);
+    % log %
+    fprintf('phoneNumber%d', index);
+    disp(phoneNumber);
+    if valid && length(phoneNumber) == 10
+        disp('valid')
+    else
+        disp('invalid')
+    end
 
-[signal3] = audioread('numeros_mysteres/numero_mystere3.wav');
-phoneNumber3 = getPhoneNumberFromSignal(signal3);
+    % spectogram %
+    figure(index), specgram(signal, echantillonsParBloc, frequence), title(sprintf('Spectogramme pour le numéro de téléphone %d', index));
+end
 
-fprintf('phoneNumber3');
-disp(phoneNumber3);
-
-function [phoneNumber] = getPhoneNumberFromSignal(signal)
+function [phoneNumber] = getPhoneNumberFromSignal(signal, echantillonsParBloc, blocsParSeconde)
     touches = {'1', '2', '3', 'A'; '4', '5', '6', 'B'; '7', '8', '9', 'C'; '*', '0', '#', 'D'};
     lowPks = []
     highPks = []
-    for sectionIndex = 0:floor(length(signal)/512)
-        sectionStart = max(sectionIndex*512, 1);
-        sectionEnd = min((sectionIndex+1)*512, length(signal));
-        [lowPk, highPk] = find2Pk(abs(fft(signal(sectionStart:sectionEnd))));
+    for sectionIndex = 0:floor(length(signal)/echantillonsParBloc)
+        sectionStart = max(sectionIndex*echantillonsParBloc, 1);
+        sectionEnd = min((sectionIndex+1)*echantillonsParBloc, length(signal));
+        [lowPk, highPk] = find2Pk(abs(fft(signal(sectionStart:sectionEnd))), blocsParSeconde);
         lowPks = [lowPks, lowPk];
         highPks = [highPks, highPk];
     end
@@ -33,21 +49,26 @@ function [phoneNumber] = getPhoneNumberFromSignal(signal)
 
     phoneNumber = [];
     for toucheIndex = 1:length(touchesLowFrequences)
-        lowIndex = getFrequenceIndex([697, 770, 852, 941], touchesLowFrequences{toucheIndex});
-        highIndex = getFrequenceIndex([1209, 1336, 1477, 1633], touchesHighFrequences{toucheIndex});
-        phoneNumber = [phoneNumber, touches(lowIndex,highIndex)];
+        lowIndex = getFrequenceIndex([697, 770, 852, 941], touchesLowFrequences{toucheIndex}, blocsParSeconde);
+        highIndex = getFrequenceIndex([1209, 1336, 1477, 1633], touchesHighFrequences{toucheIndex}, blocsParSeconde);
+        if lowIndex && highIndex
+            foundedTouche = touches(lowIndex, highIndex);
+            phoneNumber = [phoneNumber, touches(lowIndex,highIndex)];
+        else
+            disp('Invalid key')
+        end
     end
 end
 
-function [lowPk, highPk] = find2Pk(section)
+function [lowPk, highPk] = find2Pk(section, blocsParSeconde)
     [~, pksLoc] = findpeaks(section);
     if length(pksLoc) > 0
-        lowPk = (pksLoc(1)-1)*15.625;
+        lowPk = (pksLoc(1)-1)*blocsParSeconde;
     else
         lowPk = 0;
     end
     if length(pksLoc) > 1
-        highPk = (pksLoc(2)-1)*15.625;
+        highPk = (pksLoc(2)-1)*blocsParSeconde;
     else
         highPk = 0;
     end
@@ -60,13 +81,13 @@ function [touchesPks] = findTouchesPks(pks)
     ix1 = ne0([find(diff([0 ne0])>1)-1 length(ne0)]);
     touchesPks = [];
     for k1 = 1:length(ix0)
-        touchesPks{k1} = max(pks(ix0(k1):ix1(k1)));
+        touchesPks{k1} = mode(pks(ix0(k1):ix1(k1)));
     end
 end
 
-function [index] = getFrequenceIndex(frequences, frequence)
+function [index] = getFrequenceIndex(frequences, frequence, blocsParSeconde)
     [c, index] = min(abs(frequences-frequence));
-    if c > 15.625
+    if c > blocsParSeconde
         index = 0;
     end
 end
