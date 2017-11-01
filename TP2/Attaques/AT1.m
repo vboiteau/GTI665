e880c930b7b7744ac91c9d3ff1596c09ad7f93f7
Andres Ponce de Leon <andponcedeleon@gmail.com>
2017-11-01 18:41:11 -0400

    attaques patchwork

--- TP2/Attaques/AT1.m
+++ TP2/Attaques/AT1.m
@@ -1,8 +1,8 @@
 path_array = strsplit(mfilename('fullpath'), '/');
 basePath = strjoin(path_array(1:length(path_array) - 1), '/');
-INPUT_PATH = sprintf('%s/../Media/lena.ppm', basePath);
-mkdir(sprintf('%s/../Media/output/attaques/AT1', basePath));
+INPUT_PATH = sprintf('%s/../Patchwork/patchwork.ppm', basePath);
+mkdir(sprintf('%s/../Media/output/attaques/patchwork/AT1', basePath));
 mkdir(sprintf('%s/../Media/tmp/', basePath));
-OUTPUT_PATH = sprintf('%s/../Media/output/attaques/AT1/lena.jpg', basePath);
+OUTPUT_PATH = sprintf('%s/../Media/output/attaques/patchwork/AT1/patchworkAT1.jpg', basePath);
 original = imread(INPUT_PATH);
 imwrite(original, OUTPUT_PATH, 'Quality', 10);
--- TP2/Attaques/AT3.m
+++ TP2/Attaques/AT3.m
@@ -1,9 +1,9 @@
 path_array = strsplit(mfilename('fullpath'), '/');
 basePath = strjoin(path_array(1:length(path_array) - 1), '/');
-INPUT_PATH = sprintf('%s/../Media/lena.ppm', basePath);
-mkdir(sprintf('%s/../Media/output/attaques/AT3', basePath));
+INPUT_PATH = sprintf('%s/../Patchwork/patchwork.ppm', basePath);
+mkdir(sprintf('%s/../Media/output/attaques/patchwork/AT3', basePath));
 mkdir(sprintf('%s/../Media/tmp/', basePath));
-OUTPUT_PATH = sprintf('%s/../Media/output/attaques/AT3/lena.bmp', basePath);
+OUTPUT_PATH = sprintf('%s/../Media/output/attaques/patchwork/AT3/patchworkAT3.bmp', basePath);
 original = imread(INPUT_PATH);
 imageRot = imrotate(original, 90);
 imwrite(imageRot, OUTPUT_PATH);
