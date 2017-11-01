7c5a37a287e2a014cba435e463a783201f0e32aa
Andres Ponce de Leon <andponcedeleon@gmail.com>
2017-11-01 19:09:07 -0400

    Patchwork fini

--- TP2/Patchwork/patchwork.m
+++ TP2/Patchwork/patchwork.m
@@ -58,15 +58,87 @@ for i=1:10000
 end
 Sn2 = round(Sn2*256);
 fprintf('Sn image tatouee %i\n', Sn2);
+
+AT1(imageTatouee,keyAx,keyAy,keyBx,keyBy);
+AT2(imageTatouee,keyAx,keyAy,keyBx,keyBy);
+AT3(imageTatouee,keyAx,keyAy,keyBx,keyBy);
+%AT4(imageTatouee,keyAx,keyAy,keyBx,keyBy); Marche pas car les index ne
+%sont plus les memes (la dimension a change)
+
+function AT1(image,keyAx,keyAy,keyBx,keyBy)
+    imwrite(image, 'attaque1.jpg', 'Quality', 10);
+    attaque = imread('attaque1.jpg');
+    attaqueHSV=rgb2hsv(attaque);
+    Sn = 0;
+    for i=1:10000
+       diff = double(attaqueHSV(keyAx(i),keyAy(i),3)) - double(attaqueHSV(keyBx(i),keyBy(i),3));
+       Sn = Sn + diff;
+    end
+    Sn = round(Sn*256);
+    fprintf('Sn image tatouee AT1: %i\n', Sn);
+end
+
+function AT2(image,keyAx,keyAy,keyBx,keyBy)
+    imageNoise = imnoise(image, 'salt & pepper', 0.1);
+    imwrite(imageNoise, 'attaque2.jpg');
+    
+    attaque = imread('attaque2.jpg');
+    attaqueHSV=rgb2hsv(attaque);
+    Sn = 0;
+    for i=1:10000
+       diff = double(attaqueHSV(keyAx(i),keyAy(i),3)) - double(attaqueHSV(keyBx(i),keyBy(i),3));
+       Sn = Sn + diff;
+    end
+    Sn = round(Sn*256);
+    fprintf('Sn image tatouee AT2: %i\n', Sn);
+end
+
+function AT3(image,keyAx,keyAy,keyBx,keyBy)
+    imageRot = imrotate(image, 90);
+    imwrite(imageRot, 'attaque3.jpg');
+    
+    attaque = imread('attaque3.jpg');
+    attaqueHSV=rgb2hsv(attaque);
+    Sn = 0;
+    for i=1:10000
+       diff = double(attaqueHSV(keyAx(i),keyAy(i),3)) - double(attaqueHSV(keyBx(i),keyBy(i),3));
+       Sn = Sn + diff;
+    end
+    Sn = round(Sn*256);
+    fprintf('Sn image tatouee AT3: %i\n', Sn);
+end
+
+function AT4(image,keyAx,keyAy,keyBx,keyBy)
+    
+    [width, height, color] = size(image);
+    newWidth = width * 0.75;
+    newHeight = height * 0.75;
+    newX = (width - newWidth) / 2;
+    newY = (height - newHeight) / 2;
+    imageCropped = imcrop(image, [newX newY (newX + newWidth) (newY + newHeight)]);
+    imwrite(imageCropped, 'attaque4.jpg');
+    
+    attaque = imread('attaque4.jpg');
+    attaqueHSV=rgb2hsv(attaque);
+    Sn = 0;
+    for i=1:10000
+       diff = double(attaqueHSV(keyAx(i),keyAy(i),3)) - double(attaqueHSV(keyBx(i),keyBy(i),3));
+       Sn = Sn + diff;
+    end
+    Sn = round(Sn*256);
+    fprintf('Sn image tatouee AT4: %i\n', Sn);
+    
+end
+
 %disp(std2(imageTatouee));
 %disp(mean2(imageTatouee));
 
-for i=1:10000   
-   imageHSV(keyAx(i),keyAy(i),3) = imageHSV(keyAx(i),keyAy(i),3) - Delta;
-   imageHSV(keyBx(i),keyBy(i),3) = imageHSV(keyBx(i),keyBy(i),3) + Delta;
-end
+%for i=1:10000   
+%   imageHSV(keyAx(i),keyAy(i),3) = imageHSV(keyAx(i),keyAy(i),3) - Delta;
+%   imageHSV(keyBx(i),keyBy(i),3) = imageHSV(keyBx(i),keyBy(i),3) + Delta;
+%end
 
-finalImgExtraction=hsv2rgb(imageHSV);
-figure(5), imshow(finalImgExtraction);
+%finalImgExtraction=hsv2rgb(imageHSV);
+%figure(5), imshow(finalImgExtraction);
 %disp(std2(finalImgExtraction));
 %disp(mean2(finalImgExtraction));
