8faff3aa74b982be2c7ef6e82c9fb26141d3f413
Andres Ponce de Leon <andponcedeleon@gmail.com>
2017-11-23 18:01:44 -0500

    shrp fonction

--- TP3/encoder.m
+++ TP3/encoder.m
@@ -1,13 +1,19 @@
 input = './Medias/wav/man.wav';
 %input = './Medias/wav/woman.wav';
-info = audioinfo(input)
+info = audioinfo(input);
 signal = audioread(input);
 chunks = splitSignal(signal);
-disp(length(chunks))
+disp("Chunks: "+length(chunks));
 for i = 1:length(chunks)
     chunk = chunks(i,:);
     isSilence = detectSilence(chunk);
-    if isSilence
+    if isSilence == 0
         isVoiced = detectVoiced(chunk);
+        
+        if isVoiced
+            [f0_time,f0_value,SHR,f0_candidates] = shrp(chunk,8000,[50 250],22.5,10,0.4,1250,0,1)
+            
+        end
+        
     end
 end
